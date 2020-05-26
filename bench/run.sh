11ce74f5b51ba0924da51aff9baf4fa833b54984
Dan Shechter <125730+damageboy@users.noreply.github.com>
2020-05-26 13:15:41 +0300

    Resume on ctrl-c

--- bench/run.sh
+++ bench/run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 hogs=$(pgrep -if "(typora|firefox|chrome|chromium-browser|rider|pycharm|resharper|msbuild|telegram|clion|discord|slack)")
-echo Suspending $(echo $hogs | wc -w) procs before running bench
-[[ -z "$hogs" ]] || echo $hogs | xargs kill -STOP
+
+resume() {
+  echo Resuming "$(echo "$hogs" | wc -w)" procs after running bench
+  [[ -z "$hogs" ]] || echo "$hogs" | xargs kill -CONT 
+}
+
+trap 'resume' SIGINT
+
+echo Suspending "$(echo "$hogs" | wc -w)" procs before running bench
+[[ -z "$hogs" ]] || echo "$hogs" | xargs kill -STOP
 
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-$SCRIPT_DIR/gcsort_bench --benchmark_counters_tabular "$@"
-echo Resuming $(echo $hogs | wc -w) procs after running bench
-[[ -z "$hogs" ]] || echo $hogs | xargs kill -CONT 
+"$SCRIPT_DIR"/gcsort_bench --benchmark_counters_tabular "$@"
+trap '' SIGINT
+resume
