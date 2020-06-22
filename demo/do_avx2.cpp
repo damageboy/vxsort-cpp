fcf046b3f33a37b53424590dad4be84a88733343
Dan Shechter <125730+damageboy@users.noreply.github.com>
2020-06-22 13:47:02 +0300

    Update workflows

--- demo/do_avx2.cpp
+++ demo/do_avx2.cpp
@@ -1,18 +1,12 @@
 
 #include "vxsort_targets_enable_avx2.h"
 
-#include <vector>
 #include "vxsort.h"
 #include "machine_traits.avx2.h"
 #include "smallsort/bitonic_sort.AVX2.int32_t.generated.h"
 
-namespace vxsort_demo {
-void do_avx2(std::vector<int>& V) {
-  auto begin = V.data();
-  auto end = V.data() + V.size() - 1;
-
+void do_avx2(int *begin, int *end) {
   auto sorter = vxsort::vxsort<int, vxsort::vector_machine::AVX2, 8>();
   sorter.sort(begin, end);
-}
 }
 #include "vxsort_targets_disable.h"
--- demo/do_avx512.cpp
+++ demo/do_avx512.cpp
@@ -1,17 +1,11 @@
 #include "vxsort_targets_enable_avx512.h"
 
-#include <vector>
 #include "vxsort.h"
 #include "machine_traits.avx512.h"
 #include "smallsort/bitonic_sort.AVX512.int32_t.generated.h"
 
-namespace vxsort_demo {
-void do_avx512(std::vector<int>& V) {
-  auto begin = V.data();
-  auto end = V.data() + V.size() - 1;
-
+void do_avx512(int *begin, int *end) {
   auto sorter = vxsort::vxsort<int, vxsort::vector_machine::AVX512, 8>();
   sorter.sort(begin, end);
-}
 }
 #include "vxsort_targets_disable.h"
