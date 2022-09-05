e54add820f9d695b032304b45488ef0c16a0aad8
damageboy <125730+damageboy@users.noreply.github.com>
2022-09-05 20:01:46 +0300

    Make sure bitonic_sort benchmarks run for some minimal time

--- bench/smallsort/BM_smallsort.avx2.cpp
+++ bench/smallsort/BM_smallsort.avx2.cpp
@@ -11,14 +11,14 @@ using namespace vxsort::types;
 using benchmark::TimeUnit;
 using vm = vxsort::vector_machine;
 
-BENCHMARK_TEMPLATE(BM_bitonic_sort, i16, vm::AVX2)->DenseRange(16, 4096, 8)->Unit(kNanosecond);
-BENCHMARK_TEMPLATE(BM_bitonic_sort, u16, vm::AVX2)->DenseRange(16, 4096, 8)->Unit(kNanosecond);
-BENCHMARK_TEMPLATE(BM_bitonic_sort, i32, vm::AVX2)->DenseRange( 4, 2048, 4)->Unit(kNanosecond);
-BENCHMARK_TEMPLATE(BM_bitonic_sort, u32, vm::AVX2)->DenseRange( 4, 2048, 4)->Unit(kNanosecond);
-BENCHMARK_TEMPLATE(BM_bitonic_sort, f32, vm::AVX2)->DenseRange( 4, 2048, 4)->Unit(kNanosecond);
-BENCHMARK_TEMPLATE(BM_bitonic_sort, i64, vm::AVX2)->DenseRange( 2, 1024, 2)->Unit(kNanosecond);
-BENCHMARK_TEMPLATE(BM_bitonic_sort, u64, vm::AVX2)->DenseRange( 2, 1024, 2)->Unit(kNanosecond);
-BENCHMARK_TEMPLATE(BM_bitonic_sort, f64, vm::AVX2)->DenseRange( 2, 1024, 2)->Unit(kNanosecond);
+BENCHMARK_TEMPLATE(BM_bitonic_sort, i16, vm::AVX2)->DenseRange(16, 4096, 8)->Unit(kNanosecond)->MinTime(0.1);
+BENCHMARK_TEMPLATE(BM_bitonic_sort, u16, vm::AVX2)->DenseRange(16, 4096, 8)->Unit(kNanosecond)->MinTime(0.1);
+BENCHMARK_TEMPLATE(BM_bitonic_sort, i32, vm::AVX2)->DenseRange( 4, 2048, 4)->Unit(kNanosecond)->MinTime(0.1);
+BENCHMARK_TEMPLATE(BM_bitonic_sort, u32, vm::AVX2)->DenseRange( 4, 2048, 4)->Unit(kNanosecond)->MinTime(0.1);
+BENCHMARK_TEMPLATE(BM_bitonic_sort, f32, vm::AVX2)->DenseRange( 4, 2048, 4)->Unit(kNanosecond)->MinTime(0.1);
+BENCHMARK_TEMPLATE(BM_bitonic_sort, i64, vm::AVX2)->DenseRange( 2, 1024, 2)->Unit(kNanosecond)->MinTime(0.1);
+BENCHMARK_TEMPLATE(BM_bitonic_sort, u64, vm::AVX2)->DenseRange( 2, 1024, 2)->Unit(kNanosecond)->MinTime(0.1);
+BENCHMARK_TEMPLATE(BM_bitonic_sort, f64, vm::AVX2)->DenseRange( 2, 1024, 2)->Unit(kNanosecond)->MinTime(0.1);
 
 }
 
