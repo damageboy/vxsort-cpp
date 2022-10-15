b4e24ca544c7ecb7e5fc0f4aba3f43fa5252e525
damageboy <125730+damageboy@users.noreply.github.com>
2022-10-15 13:19:14 +0300

    add direct partition machine tests

---
+++ tests/mini_tests/partition_machine.avx2.cpp
@@ -0,0 +1,23 @@
+#include "vxsort_targets_enable_avx2.h"
+
+#include <partition_machine.avx2.h>
+
+#include "partition_machine_test.h"
+
+namespace vxsort_tests {
+using namespace vxsort::types;
+using VM = vxsort::vector_machine;
+
+template<typename T>
+using PartitionMachineAVX2Test = PageWithLavaBoundariesFixture<T, VM::AVX2>;
+
+using MaskedTypes = ::testing::Types</*i16, u16, */i32, u32, i64, u64>;
+TYPED_TEST_SUITE(PartitionMachineAVX2Test, MaskedTypes);
+
+TYPED_TEST(PartitionMachineAVX2Test, PartitioningWorks) {
+    test_partition<TypeParam , VM::AVX2>(this);
+}
+
+};
+
+#include "vxsort_targets_disable.h"
---
+++ tests/mini_tests/partition_machine.avx512.cpp
@@ -0,0 +1,24 @@
+#include "vxsort_targets_enable_avx512.h"
+
+#include <partition_machine.avx512.h>
+#include <vector_machine/machine_traits.avx512.h>
+
+#include "partition_machine_test.h"
+
+namespace vxsort_tests {
+using namespace vxsort::types;
+using VM = vxsort::vector_machine;
+
+template<typename T>
+using PartitionMachineAVX512Test = PageWithLavaBoundariesFixture<T, VM::AVX512>;
+
+using MaskedTypes = ::testing::Types<i16, u16, i32, u32, i64, u64>;
+TYPED_TEST_SUITE(PartitionMachineAVX512Test, MaskedTypes);
+
+TYPED_TEST(PartitionMachineAVX512Test, PartitioningWorks) {
+    test_partition<TypeParam , VM::AVX512>(this);
+}
+
+};
+
+#include "vxsort_targets_disable.h"
