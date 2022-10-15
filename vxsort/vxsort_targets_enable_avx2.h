bec5d3a7f9c5788809665fa5864b4a97357afd48
damageboy <125730+damageboy@users.noreply.github.com>
2022-10-15 12:18:27 +0300

    Add popcnt,bmi2 to target requirements for clang/gcc

--- vxsort/vxsort_targets_enable_avx2.h
+++ vxsort/vxsort_targets_enable_avx2.h
@@ -2,11 +2,11 @@
 
 #if defined(VXSORT_COMPILER_CLANG) || defined(VXSORT_COMPILER_CLANGCL)
 #define VXSORT_TARGET_PUSHED 1
-#pragma clang attribute push (__attribute__((target("avx2,popcnt"))), apply_to = any(function))
+#pragma clang attribute push (__attribute__((target("avx2,popcnt,bmi2"))), apply_to = any(function))
 #endif
 
 #if defined(VXSORT_COMPILER_GCC)
 #define VXSORT_TARGET_PUSHED 1
 #pragma GCC push_options
-#pragma GCC target("avx2")
+#pragma GCC target("avx2,popcnt,bmi2")
 #endif
