02991b6ee6a91a516d6d3ebf210068dbfe6d8ea6
Dan Shechter <dans@houmus.org>
2022-10-26 15:41:55 +0300

    Enable i16 vxsort testing since it finally works

--- bench/fullsort/BM_fullsort.avx2.cpp
+++ bench/fullsort/BM_fullsort.avx2.cpp
@@ -12,6 +12,11 @@ using namespace vxsort::types;
 using benchmark::TimeUnit;
 using vm = vxsort::vector_machine;
 
+BENCHMARK_TEMPLATE(BM_vxsort, i16, vm::AVX2,  1)->RangeMultiplier(2)->Range(MIN_SORT, MAX_SORT)->Unit(kMillisecond)->ThreadRange(1, processor_count);
+BENCHMARK_TEMPLATE(BM_vxsort, i16, vm::AVX2,  4)->RangeMultiplier(2)->Range(MIN_SORT, MAX_SORT)->Unit(kMillisecond)->ThreadRange(1, processor_count);
+BENCHMARK_TEMPLATE(BM_vxsort, i16, vm::AVX2,  8)->RangeMultiplier(2)->Range(MIN_SORT, MAX_SORT)->Unit(kMillisecond)->ThreadRange(1, processor_count);
+BENCHMARK_TEMPLATE(BM_vxsort, i16, vm::AVX2, 12)->RangeMultiplier(2)->Range(MIN_SORT, MAX_SORT)->Unit(kMillisecond)->ThreadRange(1, processor_count);
+
 BENCHMARK_TEMPLATE(BM_vxsort, i32, vm::AVX2,  1)->RangeMultiplier(2)->Range(MIN_SORT, MAX_SORT)->Unit(kMillisecond)->ThreadRange(1, processor_count);
 BENCHMARK_TEMPLATE(BM_vxsort, i32, vm::AVX2,  4)->RangeMultiplier(2)->Range(MIN_SORT, MAX_SORT)->Unit(kMillisecond)->ThreadRange(1, processor_count);
 BENCHMARK_TEMPLATE(BM_vxsort, i32, vm::AVX2,  8)->RangeMultiplier(2)->Range(MIN_SORT, MAX_SORT)->Unit(kMillisecond)->ThreadRange(1, processor_count);
