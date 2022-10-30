bdecfa03f510d15f5867d778c090019ff9a187b3
damageboy <125730+damageboy@users.noreply.github.com>
2022-10-30 09:40:07 +0200

    Add BM_bitonic_machine to test the raw speed of 1/2 vector sorting

--- bench/smallsort/BM_smallsort.h
+++ bench/smallsort/BM_smallsort.h
@@ -22,6 +22,8 @@ static void BM_bitonic_sort(benchmark::State& state) {
         return;
     }
 
+    using BM = vxsort::smallsort::bitonic<Q, M>;
+
     static const i32 ITERATIONS = 1024;
     auto n = state.range(0);
     auto v = std::vector<Q>(n);
@@ -37,7 +39,7 @@ static void BM_bitonic_sort(benchmark::State& state) {
         state.ResumeTiming();
         auto start = cycleclock::Now();
         for (auto i = 0; i < ITERATIONS; i++)
-            vxsort::smallsort::bitonic<Q, M>::sort(begins[i], n);
+            BM::sort(begins[i], n);
         total_cycles += cycleclock::Now() - start;
     }
 
@@ -47,7 +49,53 @@ static void BM_bitonic_sort(benchmark::State& state) {
     process_perf_counters(state.counters, n * ITERATIONS);
     if (!state.counters.contains("cycles/N"))
         state.counters["rdtsc-cycles/N"] = make_cycle_per_n_counter((f64)total_cycles / (f64)(n * ITERATIONS * state.iterations()));
+}
 
+template <class Q, vxsort::vector_machine M, int N>
+static void BM_bitonic_machine(benchmark::State& state) {
+    static_assert(N > 0, "N must be greater than 0");
+    static_assert(N <= 4, "N cannot exceet 4");
+    if (!vxsort::supports_vector_machine(M)) {
+        state.SkipWithError("Current CPU does not support the minimal features for this test");
+        return;
+    }
+
+    using BM = vxsort::smallsort::bitonic_machine<Q, M>;
+
+    static const i32 ITERATIONS = 1024;
+    auto n = N * BM::N;
+    auto v = std::vector<Q>(n);
+    generate_unique_values_vec(v, (Q)0x1000, (Q)0x8);
+
+    auto copies = generate_copies(ITERATIONS, n, v);
+    auto begins = generate_array_beginnings(copies);
+
+    uint64_t total_cycles = 0;
+    for (auto _ : state) {
+        state.PauseTiming();
+        refresh_copies(copies, v);
+        state.ResumeTiming();
+        auto start = cycleclock::Now();
+        for (auto i = 0; i < ITERATIONS; i++) {
+            if (N == 1)
+                BM::sort_01v_full_ascending(begins[i]);
+            else if (N == 2)
+                BM::sort_02v_full_ascending(begins[i]);
+            else if (N == 3)
+                BM::sort_03v_full_ascending(begins[i]);
+            else if (N == 4)
+                BM::sort_04v_full_ascending(begins[i]);
+
+        }
+        total_cycles += cycleclock::Now() - start;
+    }
+
+    state.SetBytesProcessed(state.iterations() * n * ITERATIONS * sizeof(Q));
+
+    state.counters["Time/N"] = make_time_per_n_counter(n * ITERATIONS);
+    process_perf_counters(state.counters, n * ITERATIONS);
+    if (!state.counters.contains("cycles/N"))
+        state.counters["rdtsc-cycles/N"] = make_cycle_per_n_counter((f64)total_cycles / (f64)(n * ITERATIONS * state.iterations()));
 }
 
 }
