076f991e871048bed68023056fbf42c878105837
damageboy <125730+damageboy@users.noreply.github.com>
2022-10-31 07:48:17 +0200

    Cleanup tests so that they are more uniform, remove a useless std::vector copy

--- tests/fullsort/fullsort_test.h
+++ tests/fullsort/fullsort_test.h
@@ -25,45 +25,44 @@ void vxsort_test(std::vector<T>& V) {
         return;
     }
 
-    auto copy = std::vector<T>(V);
+    auto v_copy = std::vector<T>(V);
     auto begin = V.data();
     auto end = V.data() + V.size() - 1;
 
     auto sorter = ::vxsort::vxsort<T, M, Unroll>();
     sorter.sort(begin, end);
 
-    std::sort(copy.begin(), copy.end());
-    usize size = copy.size();
+    std::sort(v_copy.begin(), v_copy.end());
+    usize size = v_copy.size();
     for (usize i = 0; i < size; ++i) {
-        if (copy[i] != V[i]) {
-            GTEST_FAIL() << fmt::format("value at idx #{}  {} != {}", i, copy[i], V[i]);
+        if (v_copy[i] != V[i]) {
+            GTEST_FAIL() << fmt::format("value at idx #{}  {} != {}", i, v_copy[i], V[i]);
         }
     }
 }
 
 template <typename T, i32 Unroll, int Shift, vector_machine M>
-void vxsort_hinted_test(std::vector<T> V, T min_value, T max_value) {
+void vxsort_hinted_test(std::vector<T>& V, T min_value, T max_value) {
     if (!::vxsort::supports_vector_machine(M)) {
         GTEST_SKIP_(
                 "Current CPU does not support the minimal features for this test");
         return;
     }
 
-    auto copy = std::vector<T>(V);
-
+    auto v_copy = std::vector<T>(V);
     auto begin = V.data();
     auto end = V.data() + V.size() - 1;
 
     auto sorter = ::vxsort::vxsort<T, M, Unroll, Shift>();
     sorter.sort(begin, end, min_value, max_value);
 
-    std::sort(copy.begin(), copy.end());
+    std::sort(v_copy.begin(), v_copy.end());
 
     //EXPECT_THAT(V, WhenSorted(ElementsAreArray(V)));
-    u64 size = copy.size();
+    u64 size = v_copy.size();
     for (auto i = 0ULL; i < size; ++i) {
-        if (copy[i] != V[i]) {
-            GTEST_FAIL() << fmt::format("value at idx #{}  {} != {}", i, copy[i], V[i]);
+        if (v_copy[i] != V[i]) {
+            GTEST_FAIL() << fmt::format("value at idx #{}  {} != {}", i, v_copy[i], V[i]);
         }
     }
 
